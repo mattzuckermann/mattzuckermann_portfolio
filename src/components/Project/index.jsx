2cfc5fd588b0b3cf81bce374bbd32e3cd027891f
Matt Zuckermann <matt@mattzuckermann.dev>
2021-09-26 08:55:31 -0700

    refactor: :fire: removes demo username and password and corresponding logic per move to autofill form in each app individually

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react"
-import { useSpring, animated } from "react-spring"
-import navigator from "../../js/navigator"
-import imageInfo from "../../js/imageInfo"
-import DemoLineNYT from "../DemoLineNYT"
-import DemoLineSnippets from "../DemoLineSnippets"
-import SlideAnimation from "../SlideAnimation"
+import { animated, useSpring } from "react-spring"
 import githubIcon from "../../images/icons/github.webp"
+import imageInfo from "../../js/imageInfo"
+import navigator from "../../js/navigator"
+import SlideAnimation from "../SlideAnimation"
 
 const Project = ({ fade, index }) => {
   const [mouseIn, hoverChange] = useState(false)
@@ -80,21 +78,6 @@ const Project = ({ fade, index }) => {
               </span>
             </div>
             <div className="shortBio">{imageInfo[index].shortBio}</div>
-          </div>
-          {imageInfo[index].extraHeight === 1 && (
-            <div>
-              <br />
-            </div>
-          )}
-          {imageInfo[index].extraHeight === 2 && (
-            <div>
-              <br />
-              <br />
-            </div>
-          )}
-          <div className="demoDiv">
-            {imageInfo[index].appName === "NYT Searcher" && <DemoLineNYT />}
-            {imageInfo[index].appName === "Snippets." && <DemoLineSnippets />}
           </div>
         </div>
       </animated.div>
--- src/js/imageInfo.js
+++ src/js/imageInfo.js
@@ -14,7 +14,6 @@ export default [
     desc:
       "JAMstack app for developers in mastering the use of the “this” keyword in various JavaScript contexts",
     image: useThisImage,
-    extraHeight: 1,
   },
   {
     appName: "Music Artist Site",
@@ -24,7 +23,6 @@ export default [
     desc:
       "JAMstack site with reverse engineered Spotify audio player and webhook to auto-compile site from CMS",
     image: jzMusicImage,
-    extraHeight: 1,
   },
   {
     appName: "NYT Searcher",
@@ -34,7 +32,6 @@ export default [
     desc:
       "Website that parses The New York Times articles API and allows users to share their favorites with others",
     image: bestSellerSearcherImage,
-    extraHeight: 0,
   },
   {
     appName: "Snippets.",
@@ -45,7 +42,6 @@ export default [
     desc:
       "Snippets is a personalized user database to store code, displaying it in a colored and formatted manner.",
     image: codesnippetsImage,
-    extraHeight: 0,
   },
   {
     appName: "Liri Bot",
@@ -55,7 +51,6 @@ export default [
     desc:
       "Node app that inputs varying methods and arguments from Node CLI to look up various web API end points.",
     image: liriBotImage,
-    extraHeight: 2,
   },
   {
     appName: "Store Stock",
@@ -65,6 +60,5 @@ export default [
     desc:
       "Node app that prompts users with items for sale and tracks made purchases using a MySQL database.",
     image: storeStockImage,
-    extraHeight: 2,
   },
 ]
